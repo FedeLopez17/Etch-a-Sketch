605f9d8742834a0211bd38a8c8d903c355643a4d
FedeLopez17 <fedelopez1712@gmail.com>
2022-04-30 15:09:14 -0300

    App overhaul, mainly aesthetically

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,92 +1,59 @@
 const sketchContainer = document.querySelector(".sketch-container");
-const grid = document.createElement("div");
-grid.classList.add("grid");
-grid.addEventListener("mousedown", ()=>{draw = true});
-grid.addEventListener("mouseup", ()=>{draw = false});
-const knobs = document.querySelector(".knobs");
-sketchContainer.insertBefore(grid, knobs);
-let draw = false; size = 64; color = "black"; eraserActivated = false; rainbowSelected = false;
+sketchContainer.classList.toggle("slide-in-animation");
+const gridContainer = document.querySelector(".grid-container");
+const controls = document.querySelector(".controls");
+for (let control of controls.childNodes){
+    control.addEventListener("mouseenter", ()=>{control.classList.toggle("mouse-over");});
+    control.addEventListener("mouseleave", ()=>{control.classList.toggle("mouse-over");});
+}
+gridContainer.addEventListener("mousedown", ()=>{draw = true});
+gridContainer.addEventListener("mouseup", ()=>{draw = false});
+
+let draw = false; size = 18; color = "#595959"; eraserActivated = false; rainbowSelected = false; grid = true;
 let oldColor;
 
-
 function removeGrid(){
-    const horizontalContainers = document.querySelectorAll(".horizontalContainers");
+    const horizontalContainers = document.querySelectorAll(".horizontal-divs");
     while (horizontalContainers.firstChild){
         horizontalContainers.removeChild(horizontalContainers.firstChild);
     }
-    while (grid.firstChild){
-        grid.removeChild(grid.firstChild);
+    while (gridContainer.firstChild){
+        gridContainer.removeChild(gridContainer.firstChild);
     }
     return;
 }
-// Calls the function which makes a new grid and passes it the selected density as its argument;
+// Deletes current gird and calls the function which makes a new grid and passes it the selected density as its argument;
 function selectGridDensity(e){
-    if (this.checked){
-        removeGrid();
-        size = Number(this.value);
-        makeGrid(size);
-        return;
-    }
+    removeGrid();
+    size = Number(this.value);
+    makeGrid(size);
+    return;
+    
 }
 
-const radioButtonsGrid = document.querySelectorAll("input[name='grid-density']");
-for (const button of radioButtonsGrid){
-    button.addEventListener("change", selectGridDensity);
-}
+const sliderGrid = document.querySelector("input[name='grid-density']")
+sliderGrid.addEventListener("change", selectGridDensity);
 
 function makeGrid (size) {
     for (let i = 0; i < size; i++){
-        let horizontalContainer = document.createElement("div");
-        horizontalContainer.classList.add("horizontalContainers");
-        grid.appendChild(horizontalContainer);
-        for (let j = 0; j < size; j++){
-            let square = document.createElement("div");
+        const horizontalDiv = document.createElement("div");
+        horizontalDiv.classList.add("horizontal-divs");
+        gridContainer.append(horizontalDiv);
+        for (let j = 0; j < (size * 2); j++){
+            const square = document.createElement("div");
             square.classList.add("squares");
-            switch (size){
-                case 64:
-                    square.style.cssText = "padding: 4.82px;";
-                    break;
-                case 32: 
-                    square.style.cssText = "padding: 10.13px;";
-                    break;
-                case 16: 
-                    square.style.cssText = "padding: 20.75px;";
-                    break;
-                case 8:
-                    square.style.cssText = "padding: 42px;";
+            if (!grid){
+                square.classList.add("no-grid");
             }
-            square.addEventListener("mousedown", ()=>{(!rainbowSelected) ? square.style.backgroundColor = color : square.style.backgroundColor = randomColor();});
-            square.addEventListener("mouseover", ()=> {if(draw){(!rainbowSelected) ? square.style.backgroundColor = color : square.style.backgroundColor = randomColor();}});
-            horizontalContainer.appendChild(square);
+            square.addEventListener("mousedown", ()=>{(!rainbowSelected || eraserActivated) ? square.style.backgroundColor = color : square.style.backgroundColor = randomColor();});
+            square.addEventListener("mouseover", ()=> {if(draw){(!rainbowSelected || eraserActivated) ? square.style.backgroundColor = color : square.style.backgroundColor = randomColor();}});
+            horizontalDiv.appendChild(square);
         }
     }
 }
 
 const colorSelector = document.querySelector("#color");
 colorSelector.addEventListener("change", ()=>{color = colorSelector["value"];});
-
-const eraser = document.querySelector(".eraser-wrapper");
-eraser.addEventListener("click", ()=> {
-    if (!eraserActivated){
-        oldColor = color;
-        color = "white";
-        eraserActivated = true;
-        eraser.classList.toggle("selected");
-    }
-    else{
-        color = oldColor;
-        eraserActivated = false;
-        eraser.classList.toggle("selected");
-    }
-});
-
-const clear = document.querySelector(".clear-wrapper");
-clear.addEventListener("click", ()=>{
-    clear.classList.toggle("selected");
-    removeGrid();
-    makeGrid(size);
-    setTimeout(()=>{clear.classList.toggle("selected");}, 200);
-});
 
 const rainbow = document.querySelector(".rainbow-wrapper");
 rainbow.addEventListener("click", ()=>{
@@ -114,11 +81,48 @@ function randomColor(){
     return rgbString;
 }
 
+const eraser = document.querySelector(".eraser-wrapper");
+eraser.addEventListener("click", ()=> {
+    if (!eraserActivated){
+        oldColor = color;
+        color = "white";
+        eraserActivated = true;
+        eraser.classList.toggle("selected");
+    }
+    else{
+        color = oldColor;
+        eraserActivated = false;
+        eraser.classList.toggle("selected");
+    }
+});
+
+const clear = document.querySelector(".clear-wrapper");
+clear.addEventListener("click", ()=>{
+    clear.classList.toggle("selected");
+    removeGrid();
+    makeGrid(size);
+    setTimeout(()=>{clear.classList.toggle("selected");}, 200);
+    if(sketchContainer.classList.contains("slide-in-animation")){
+        sketchContainer.classList.remove("slide-in-animation");
+    }
+    sketchContainer.classList.toggle("clear-shake-animation");
+    setTimeout(()=>{sketchContainer.classList.toggle("clear-shake-animation");}, 1000);
+});
+
+const gridToggle = document.querySelector(".gridToggle-wrapper");
+gridToggle.addEventListener("click", ()=>{
+    for(let child of gridContainer.childNodes){
+        for (let square of child.childNodes){
+            square.classList.toggle("no-grid");
+        }
+    }
+    (grid) ? grid = false : grid = true;
+    gridToggle.classList.toggle("selected");
+});
 makeGrid(size);
-
-//Que se pueda agrandar sin deformar el ancho total. que se pueda activar y desactivar la grid. balde de pintura. guardar los dibujitos. 
+//cosas para agregar a futuro 
+//balde de pintura(flood fill algorithm). guardar los dibujitos. 
 //que giren los rendodelitos. que te muestre las coordenadas donde estas parado. que la gente se pueda registrar para guardar 
-//sus dibujos en galerias personales para seguirlos dibujando mas tarde
-
-
-
+//sus dibujitos en galerias personales para seguirlos dibujando mas tarde.
+//Rick rolls en las perillas.
+//descargar el dibujo de alguna manera.
